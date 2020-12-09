d065c3cbbe9e5deaa7ce7302ed3539f25e80ab56
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2020-12-09 18:56:10 -0500

    add: acceptance tests for ipset resource

---
+++ proxmoxtf/acceptancetests/resource_virtual_environment_ipset_test.go
@@ -0,0 +1,134 @@
+package acceptancetests
+
+import (
+	"fmt"
+	"github.com/danitso/terraform-provider-proxmox/proxmox"
+	"github.com/danitso/terraform-provider-proxmox/proxmoxtf"
+	"github.com/danitso/terraform-provider-proxmox/proxmoxtf/acceptancetests/testutils"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
+)
+
+
+// Verifies that an IP set can be created and updated
+func TestAccResourceVirtualEnvironmentIPSet_CreateAndUpdate(t *testing.T) {
+	IPSetNameFirst := testutils.GenerateResourceName()
+	IPSetCIDREmpty := proxmox.VirtualEnvironmentClusterIPSetContent{}
+	noMatch := proxmox.CustomBool(true)
+	IPSetCIDR := proxmox.VirtualEnvironmentClusterIPSetContent{
+		{CIDR: "192.168.88.1", Comment: "ipset-cidr-comment"},
+		{CIDR: "192.168.88.2", Comment: "ipset-cidr-comment", NoMatch: &noMatch},
+	}
+
+	IPSetCIDRWithAlias := proxmox.VirtualEnvironmentClusterIPSetContent{
+		{CIDR: "192.168.88.4", Comment: "ipset-cidr-comment"},
+		// Alias resource is created by HclIPSetWithAliasResource
+		{CIDR: "test-alias", Comment: "ipset-cidr-comment", NoMatch: &noMatch},
+	}
+
+	IPSetFirst := proxmox.VirtualEnvironmentClusterIPSetContent{IPSetCIDR[0]}
+	IPSetSecond := proxmox.VirtualEnvironmentClusterIPSetContent{IPSetCIDR[1]}
+
+	IPSetNameSecond := testutils.GenerateResourceName()
+	tfNode := "proxmox_virtual_environment_cluster_ipset.ipset"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testutils.PreCheck(t, nil) },
+		Providers: testutils.GetProviders(),
+		CheckDestroy: CheckIPSetDestroyed,
+		Steps: []resource.TestStep{
+			// Create empty IPSet
+			{
+				Config: testutils.HclIPSetResource(IPSetNameFirst, "ipset-comment", IPSetCIDREmpty),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameFirst),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment"),
+					testutils.CheckIPSetExists(IPSetCIDREmpty),
+				),
+			},
+			// Update IPSet with CIDR
+			{
+				Config: testutils.HclIPSetResource(IPSetNameFirst, "ipset-comment", IPSetFirst),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameFirst),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment"),
+					testutils.CheckIPSetExists(IPSetFirst),
+				),
+			},
+			// Update IPSet's name and comment
+			{
+				Config: testutils.HclIPSetResource(IPSetNameSecond, "ipset-comment-updated", IPSetFirst),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameSecond),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment-updated"),
+					testutils.CheckIPSetExists(IPSetFirst),
+				),
+			},
+			// Update IPSet's CIDR
+			{
+				Config: testutils.HclIPSetResource(IPSetNameSecond, "ipset-comment-updated", IPSetSecond),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameSecond),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment-updated"),
+					testutils.CheckIPSetExists(IPSetSecond),
+				),
+			},
+			// Update IPSet with multiple CIDRs
+			{
+				Config: testutils.HclIPSetResource(IPSetNameSecond, "ipset-comment-updated", IPSetCIDR),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameSecond),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment-updated"),
+					testutils.CheckIPSetExists(IPSetCIDR),
+				),
+			},
+			// Update IPSet's include referenced alias
+			{
+				Config: testutils.HclIPSetWithAliasResource(IPSetNameSecond, "ipset-comment-updated", IPSetCIDRWithAlias),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "name", IPSetNameSecond),
+					resource.TestCheckResourceAttr(tfNode, "comment", "ipset-comment-updated"),
+					testutils.CheckIPSetExists(IPSetCIDRWithAlias),
+				),
+			},
+		},
+	})
+}
+
+// CheckIPSetDestroyed verifies that all ip sets referenced in the state
+// are destroyed. This will be invoked *after* terraform destroys
+// the resource but *before* the state is wiped clean
+func CheckIPSetDestroyed(s *terraform.State) error {
+	config := testutils.GetProvider().Meta().(proxmoxtf.ProviderConfiguration)
+	conn, err := config.GetVEClient()
+
+	if err != nil {
+		return err
+	}
+
+	// loop through the resource state
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "proxmox_virtual_environment_cluster_ipset" {
+			continue
+		}
+
+		response, err := conn.GetListIPSetContent(rs.Primary.ID)
+
+		if err == nil {
+
+			if len(response) != 0 {
+				fmt.Errorf("IPSet with name `%s` should not exist", rs.Primary.ID)
+			}
+
+			return nil
+		}
+
+		if !strings.Contains(err.Error(), "no such IPSet") {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ proxmoxtf/acceptancetests/testutils/ipset.go
@@ -0,0 +1,126 @@
+package testutils
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/danitso/terraform-provider-proxmox/proxmox"
+	"github.com/danitso/terraform-provider-proxmox/proxmoxtf"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"reflect"
+	"text/template"
+)
+
+// CheckIPSetExists Given the name of an IPSet, this will return a function that will check
+// whether or not an IPSet
+// - (1) exists in the state
+// - (2) exist in Proxmox VE
+// - (3) has the correct name
+func CheckIPSetExists(IPSetContent proxmox.VirtualEnvironmentClusterIPSetContent) resource.TestCheckFunc {
+	return func (s *terraform.State) error {
+		res, ok := s.RootModule().Resources["proxmox_virtual_environment_cluster_ipset.ipset"]
+		if !ok {
+			return fmt.Errorf("Did not find IPSet in the TF state")
+		}
+
+		clients := GetProvider().Meta().(proxmoxtf.ProviderConfiguration)
+		id := res.Primary.ID
+		ReadIPSetContent, err := readIPSet(clients, id)
+
+		if err != nil {
+			return fmt.Errorf("IPSet with Name=%s cannot be found. Error %v", id, err)
+		}
+
+		if len(IPSetContent) != len(ReadIPSetContent) {
+			return fmt.Errorf("IPSet with Name=%s has content length %v, expected %v", id, len(ReadIPSetContent), len(IPSetContent))
+		}
+
+		if len(IPSetContent) == 0 && len(ReadIPSetContent) > 0 {
+			return fmt.Errorf("IPSet with Name=%s should not have any elements in it. Found %v", id, len(ReadIPSetContent))
+		}
+
+		if len(IPSetContent) > 0 {
+
+			for i, v := range IPSetContent {
+				if v.CIDR != ReadIPSetContent[i].CIDR {
+					return fmt.Errorf("IPSet with Name=%s contains not expected IP/CIDR=%s, expected %s", id, ReadIPSetContent[i].CIDR, v.CIDR)
+				}
+
+				if v.Comment != ReadIPSetContent[i].Comment {
+					return fmt.Errorf("IPSet with Name=%s contains not expected Comment value=%s, exepected %s", id, ReadIPSetContent[i].Comment, v.Comment)
+				}
+
+				if !reflect.DeepEqual(&v.NoMatch, &ReadIPSetContent[i].NoMatch) {
+					return fmt.Errorf("IPSet with Name=%s contains not expected NoMatch=%t, expected %t", id, ReadIPSetContent[i].NoMatch, v.NoMatch)
+				}
+			}
+
+		}
+
+		return nil
+	}
+}
+
+// readIPSet is a helper function that reads an IPSet based on a given name
+func readIPSet(clients proxmoxtf.ProviderConfiguration, identifier string) ([]*proxmox.VirtualEnvironmentClusterIPSetGetResponseData, error) {
+	conn, err := clients.GetVEClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := conn.GetListIPSetContent(identifier)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// HclIPSetResource HCL describing of a PVE IPSet resource
+func HclIPSetResource(name string, comment string, cidr proxmox.VirtualEnvironmentClusterIPSetContent) string  {
+	var b bytes.Buffer
+
+	if name == "" {
+		panic("Parameter: `name` cannot be empty")
+	}
+
+	testRunData := map[string]interface{}{
+		"Name" : name,
+		"Comment": comment,
+		"CIDR" : cidr,
+	}
+
+	tmpl, err := template.New("").Parse(`
+resource "proxmox_virtual_environment_cluster_ipset" "ipset" {
+	name    = "{{.Name}}"
+	comment = "{{.Comment}}"
+	{{range .CIDR}}
+	ipset {
+		cidr = "{{.CIDR}}"
+		comment = "{{.Comment}}"
+		{{if .NoMatch}}
+		nomatch = "{{.NoMatch}}"
+		{{end}}
+	}
+	{{end}}
+}
+`)
+	t := template.Must(tmpl, err)
+	if err := t.Execute(&b, testRunData); err != nil {
+		_ = fmt.Errorf("Unable to parse template: %p", err)
+		return ""
+	}
+
+	return b.String()
+}
+
+// HclIPSetWithAliasResource HCL describing of a PVE IPSet resource with alias
+func HclIPSetWithAliasResource(name string, comment string, cidr proxmox.VirtualEnvironmentClusterIPSetContent) string  {
+	alias := HclAliasResource("test-alias", "192.168.0.0/23", "alias-comment")
+	IPSet := HclIPSetResource(name, comment, cidr)
+
+	return fmt.Sprintf("%s\n%s", alias, IPSet)
+
+}
